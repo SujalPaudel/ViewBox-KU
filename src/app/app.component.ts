d511df6c90f7e71f62fbcd81476a7fbe771eb8ec
Velar <Sujal Paudel>
2018-06-14 17:14:10 +0545

    Building components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+// where the navigation ressides, this will be accessed by every child component
+
 @Component({
+
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl:  'app.component.html' 
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Freelance Bootcamp Dashboard';
 }
+
+
+// components passed from router is going to be passed in <router-outlet>
---
+++ src/app/documents/documents.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-documents',
+  templateUrl: './documents.component.html',
+})
+export class DocumentsComponent  { }
+
+
+// decorator give the ability to add metadata without defining them in the class itself
