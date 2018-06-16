d1cb05aaae9279237976e2fd51fd43d382e69df9
Velar <Sujal Paudel>
2018-06-16 14:40:05 +0545

    Implemented cards in the document component

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -15,21 +15,21 @@ export class DocumentsComponent  {
       description: "Dadadaddaudududu",
       file_url: "http://google.com",
       updated_at: '11/11/2018',
-      image_url: "http://google.com"
+      image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg"
     },
     {
       title: "My Second Doc",
       description: "Dadadaddaudududu",
       file_url: "http://google.com",
       updated_at: '11/11/2018',
-      image_url: "http://google.com"
+      image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg"
     },
     {
       title: "My Third Doc",
       description: "Dadadaddaudududu",
       file_url: "http://google.com",
       updated_at: '11/11/2018',
-      image_url: "http://google.com"
+      image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg"
     }
 
   ]
