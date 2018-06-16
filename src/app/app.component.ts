29e684b0cfcd5a6d19d01d3cf3cf1734c13733c6
Velar <Sujal Paudel>
2018-06-16 19:58:12 +0545

    First phase of component styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,8 @@ import { Component } from '@angular/core';
 @Component({
 
   selector: 'app-root',
-  templateUrl:  'app.component.html' 
+  templateUrl:  'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
   title = 'Freelance Bootcamp Dashboard';
