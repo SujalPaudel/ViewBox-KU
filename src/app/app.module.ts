6d6cb5898cc02128f5143499ed6a88fa4613d6b7
Velar <Sujal Paudel>
2018-06-18 18:29:17 +0545

    Api connected to the frontend docs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
+import { DocumentService } from './documents/document.service';
 import { ProposalListComponent } from './proposal-list/proposal-list.component';
 import { ProposalNewComponent } from './proposal-list/proposal-new.component';
 import { ProposalShowComponent } from './proposal-list/proposal-show.component';
@@ -29,9 +31,12 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpModule,
     NgbModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    DocumentService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
